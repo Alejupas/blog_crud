32d0a4cbcd891fd14e484d7f1543f8a2aae2d3dc
Mindaugas Kapickas <mindaugas@mkp.com>
2021-05-31 09:47:59 +0300

    ejs isntall, html to ejs rename, render pages.ejs

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # blog_crud
+
+install ejs
+npm install ejs
+and register
+// register view engine
+app.ser('view engine', 'ejs')
+pakeisti folder structure.
+rename pages papke to views
+ir papkej pakeist .html to .ejs
--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const app = express();
+const path = require('path'); //kad galetume nurodyt absoliutu kelia
 
 const PORT = 3000;
 
+//register view engine
+app.set('view engine', 'ejs');
+// nustatom render view homedir
+app.set('views', 'src/views');
+
+//home page
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  //   res.sendFile(path.join(__dirname, 'pages', 'index.html'));
+
+  //paimti index.ejs faila is views  direktorijos
+  res.render('index');
 });
+
+//aout page
+app.get('/about', function (req, res) {
+  //   res.sendFile(path.join(__dirname, 'pages', 'about.html'));
+  res.render('about');
+});
+
+//blog page
+app.get('/blog', function (req, res) {
+  //   res.sendFile(path.join(__dirname, 'pages', 'blog.html'));
+  res.render('blog');
+});
+
+const staticPath = path.join(__dirname, 'static');
+//statine direktorija, css, js, img ir kitos..statiniam failam
+app.use(express.static(staticPath));
 
 // 404 case, kuri vartotojas ivede puslapi kurio nera
 app.use((req, res) => res.status(404).send('Oops, Page not found'));
