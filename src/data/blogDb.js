c0eb1dea40f472b1a5eef347f702502ed16218e6
Mindaugas Kapickas <mindaugas@mkp.com>
2021-05-31 14:06:24 +0300

    blog api nav create post , css fix

---
+++ src/data/blogDb.js
@@ -0,0 +1,52 @@
+const blogs = [
+  {
+    id: 1,
+    title: 'Pure Point Athleticism',
+    author: 'Anthony Edwards',
+    body: 'This article is about basketball athleticism',
+    date: '2021-04-21',
+    imgUrl: '#',
+  },
+  {
+    id: 2,
+    title: 'Looping for the ring',
+    author: 'Andre Wiggins',
+    body: 'This article is about the same thing as the viral videos go',
+    date: '2021-04-21',
+    imgUrl: '#',
+  },
+  {
+    id: 3,
+    title: 'March madness',
+    author: 'Luka Doncic',
+    body: 'This article is about basketball NCAA',
+    date: '2021-04-21',
+    imgUrl: '#',
+  },
+  {
+    id: 4,
+    title: '0 to Hero',
+    author: 'Kevin Garnett',
+    body: 'This article is about basketball in NBA',
+    date: '2021-04-21',
+    imgUrl: '#',
+  },
+  {
+    id: 5,
+    title: 'Kobe Bryant effect',
+    author: 'Luka Doncic',
+    body: 'This article is about basketball in LKL',
+    date: '2021-04-21',
+    imgUrl: '#',
+  },
+  {
+    id: 6,
+    title: 'Real shooting',
+    author: 'Ray Allen',
+    body: 'This article is about the art of shooting',
+    date: '2021-04-21',
+    imgUrl: '#',
+  },
+];
+
+module.exports = blogs;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const path = require('path'); //kad galetume nurodyt absoliutu kelia
+const blogs = require('./data/blogDb');
 
 const PORT = 3000;
 
@@ -8,8 +10,6 @@ const PORT = 3000;
 app.set('view engine', 'ejs');
 // nustatom render view homedir
 app.set('views', 'src/views');
-
-const blogData = require('./data/sampleBlog');
 
 //home page
 app.get('/', function (req, res) {
@@ -19,7 +19,6 @@ app.get('/', function (req, res) {
   res.render('index', {
     title: 'Home',
     page: 'home',
-    blogData,
   });
 });
 
@@ -38,6 +37,7 @@ app.get('/blog', function (req, res) {
   res.render('blog', {
     title: 'Our Blog',
     page: 'blog',
+    blogDb,
   });
 });
 
@@ -52,6 +52,19 @@ app.get('/contact', function (req, res) {
 const staticPath = path.join(__dirname, 'static');
 //statine direktorija, css, js, img ir kitos..statiniam failam
 app.use(express.static(staticPath));
+
+//create blog page /blog/create
+app.get('/blog/create', (req, res) => {
+  res.render('createBlog', {
+    title: 'Create new post',
+    page: 'createDb',
+  });
+});
+
+//blog api/ api/ blog - gauti visus postus json apvidalu
+app.get('/api/blogApi', (req, res) => {
+  response.json(blogs);
+});
 
 // 404 case, kuri vartotojas ivede puslapi kurio nera
 app.use((req, res) => res.status(404).send('Oops, Page not found'));
